70015c5ab2a2c6b48b49474068978aafebfc45cc
N1yron123 <n1yron214066@mail.ru>
2025-05-15 15:37:12 +0300

    5.2 commit

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,10 +1,9 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
 console.log('Welcome to the Brain Games!');
+import name from '../scr/cli.js';
+console.log('Answer "yes" if the number is even, otherwise answer "no".')
 
-const name = readlineSync.question('May I have your name?');
-console.log('Hello,' ,name+'!');
-console.log('Answer "yes" if the number is even, otherwise answer "no".');
 function main () {
     function getRandomInt(max) {
         return Math.floor(Math.random() *  max + 1);
